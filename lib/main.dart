b0324cfb357f1ed9f25749690ea38071bf069e7e
Bruno Pereira <brunorsp00@gmail.com>
2021-08-01 17:35:16 +0100

    Initial commit of main and controller

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:measure_color_battery_consumption/views/main_screen/main_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        backgroundColor: Colors.red,
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
---
+++ lib/views/main_screen/main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class MainScreen extends StatefulWidget {
+  MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final MainController controller = MainController();
+
+  @override
+  Widget build(BuildContext context) {
+  }
+}
