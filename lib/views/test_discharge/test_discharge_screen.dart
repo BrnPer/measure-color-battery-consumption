136a1844c6652147c1327e10d4964f72fb718bed
Bruno Pereira <brunorsp00@gmail.com>
2021-10-20 23:05:39 +0100

    Removed unnecessary components and add listen for dispose method

--- lib/views/test_discharge/test_discharge_screen.dart
+++ lib/views/test_discharge/test_discharge_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:measure_color_battery_consumption/common/widgets/color_picker.dart';
+import 'package:flutter/services.dart';
 import 'package:measure_color_battery_consumption/controllers/test_discharge_screen_controller.dart';
 import 'package:measure_color_battery_consumption/views/test_discharge/test_discharge_timer.dart';
 import 'package:measure_color_battery_consumption/views/test_discharge/test_discharge_information.dart';
@@ -17,55 +17,40 @@ class _TestDischargeState extends State<TestDischarge> {
       TestDischargeScreenController();
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return RxBuilder(builder: (_) {
       return Scaffold(
         backgroundColor: controller.color.value,
-        body: SafeArea(
-          child: Container(
-            alignment: Alignment.center,
-            child: controller.showingInformation.value == true
-                ? TestDischargeInformation(
-                    onButtonPress: controller.onConfirmStartClick)
-                : (controller.showingCountdown.value == true)
-                    ? TestDischargeTimer(
-                        onButtonPress: controller.onTimerFinished)
-                    : Flex(
-                        direction: Axis.vertical,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Flexible(
-                            flex: 2,
-                            child: Container(),
-                          ),
-                          Flexible(
-                            flex: 6,
-                            child: Center(
-                              child: RxBuilder(builder: (_) {
-                                return Text(
-                                  'Test Discharging Now \r\n\r\n ${controller.currentDischargeValue.value} mA',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      color: (controller.color.value ==
-                                              Colors.white)
-                                          ? Colors.black
-                                          : Colors.white,
-                                      fontSize: 20),
-                                );
-                              }),
-                            ),
-                          ),
-                          Flexible(
-                            flex: 2,
-                            child: ColorPicker(
-                                colors: controller.colors,
-                                selectedColor: controller.color.value,
-                                onColorClick: (color) =>
-                                    controller.changeColors(color)),
-                          ),
-                        ],
-                      ),
-          ),
+        body: Container(
+          alignment: Alignment.center,
+          child: controller.showingInformation.value == true
+              ? TestDischargeInformation(
+                  onButtonPress: controller.onConfirmStartClick)
+              : (controller.showingCountdown.value == true)
+                  ? TestDischargeTimer(
+                      onButtonPress: controller.onTimerFinished)
+                  : Center(
+                      child: RxBuilder(builder: (_) {
+                        return Text(
+                          controller.currentDischargeValue.value != null
+                              ? '${controller.currentDischargeValue.value} mA'
+                              : "Measuring...",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: (controller.color.value == Colors.white)
+                                  ? Colors.black
+                                  : Colors.white,
+                              fontSize: 20),
+                        );
+                      }),
+                    ),
         ),
       );
     });
